920bef9e3612ae1915a7376e3735d8291dd46626
Nikolay G <gurskiy@gmail.com>
2022-01-26 23:58:35 +0300

    fix: trying again;

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ module.exports = function toReadable (number) {
   if (number === 0) {
     result = 'zero';
     //return result;
-  }  else if (number <21) {
-    result = units[`${number}`];
+  }  else if (number < 21) {
+    result = units[number];
+  } else if (number > 20 && number < 100) {
+    let textNumber = number.toString();
+    result = tens[textNumber[0]] + ' ' + units[textNumber[1]];
+  } else if (number > 99 && number < 1000) {
+    let textNumber = number.toString();
+    result = units[textNumber[0]] + ' hundred ' + tens[textNumber[1]] + ' ' + units[textNumber[2]];
   }
 console.log(result);
 return result;
 }
 
-//toReadable(15);
+//toReadable(72);
