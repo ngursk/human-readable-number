509f4fee90e01a088a40d6bc7b387e2798b967d1
Nikolay G <gurskiy@gmail.com>
2022-01-26 18:13:57 +0300

    fix: 1-20

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-module.exports = function toReadable (number) {
-    const readTen = (small) => {
-        switch (small) {
-        case 0:
-          return 'zero';
-        case 1:
-          return 'one';
-        case 2:
-          return 'two';
-        case 3: 
-          return 'three';
-        case 4: 
-          return 'four';
-        case 5:
-          return 'five';
-        case 6: 
-          return 'six';
-        case 7:  
-          return 'seven';
-        case 8:
-          return 'eight';
-        case 9:
-          return 'nine';
-      }}
-    if (number.length === 1) {
-        console.log(number);
-        return readTen(number);} else 
-    if (number.length === 3) {
-    return readTen(number[0]) + ' hundred ' + readTen(number[2])}
+//module.exports = function toReadable (number) {
+const toReadable = (number) => {
+  let result = 'start';
+  units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  if (number === 0) {
+    result = 'zero';
+    //return result;
+  } else if (number < 20) {
+    switch (number) {
+      case 1: result = 'one';
+      case 2: result = 'two';
+      case 3: result = 'three';
+      case 4: result = 'four';
+      case 5: result = 'five';
+      case 6: result = 'six';
+      case 7: result = 'seven';
+      case 8: result = 'eight';
+      case 9: result = 'nine';
+      case 10: result = 'ten';
+      case 11: result = 'eleven';
+      case 12: result = 'twelve';
+      case 13: result = 'thirteen';
+      case 14: result = 'fourteen';
+      case 15: result = 'fifteen';
+      case 16: result = 'sixteen';
+      case 17: result = 'seventeen';
+      case 18: result = 'eighteen';
+      case 19: result = 'nineteen';
+      case 20: result = 'twenty';
     }
+  }
+console.log(result);
+}
+
+toReadable(12);
